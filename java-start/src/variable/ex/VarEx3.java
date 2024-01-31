098c39831c448288e769843e8e77a616da7b170b
kobe <dev.skyachieve91@gmail.com>
2024-01-31 13:15:10 +0900

    :sparkles: Feat: 변수 문제풀이.

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long bigNumber = 1000000000L;
+        boolean isTrue = true;
+
+        System.out.println(bigNumber);
+        System.out.println(isTrue);
+    }
+}
